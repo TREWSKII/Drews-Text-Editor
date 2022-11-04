51e161948f545765573e1c33b1ce366671a822eb
Drew <dlederman69@gmail.com>
2022-11-03 20:59:03 -0600

    fixed

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,4 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin/typings');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
